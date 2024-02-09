bdd75fbfbf660072568da0b824d28893838bccda
Sámal Rasmussen <samal84@gmail.com>
2024-02-09 20:35:34 +0000

    Split out nordics

--- src/routes/by-month-table/+page.svelte
+++ src/routes/by-month-table/+page.svelte
@@ -1,26 +1,33 @@
 <script>
-  import { by_month_data } from "$lib";
+  import { get_by_month_data } from "$lib";
   import groups from "$lib/country-groups.json";
 
-  const data = by_month_data;
+  const data = get_by_month_data();
   const header = /** @type {string[]} */ (data.shift());
 
+  const nordics_map = new Map(Object.entries(groups.nordics));
   const europe_map = new Map(Object.entries(groups.europe));
   const asia_map = new Map(Object.entries(groups["asia-middle-east-oceania"]));
   const africa_map = new Map(Object.entries(groups.africa));
   const americas_map = new Map(Object.entries(groups.americas));
 
+  const nordics = data.filter((row) => nordics_map.has(row[0]));
   const europe = data.filter((row) => europe_map.has(row[0]));
   const asia = data.filter((row) => asia_map.has(row[0]));
   const africa = data.filter((row) => africa_map.has(row[0]));
   const americas = data.filter((row) => americas_map.has(row[0]));
 
   console.log(
+    nordics.length,
     europe.length,
     asia.length,
     africa.length,
     americas.length,
-    europe.length + asia.length + africa.length + americas.length,
+    nordics.length +
+      europe.length +
+      asia.length +
+      africa.length +
+      americas.length,
     data.length
   );
 </script>
@@ -34,6 +41,16 @@
     </tr>
   </thead>
   <tbody>
+    <tr>
+      <td colspan={header.length}>Norðurlond</td>
+    </tr>
+    {#each nordics as row}
+      <tr>
+        {#each row as column}
+          <td class="number">{column}</td>
+        {/each}
+      </tr>
+    {/each}
     <tr>
       <td colspan={header.length}>Evropa</td>
     </tr>
