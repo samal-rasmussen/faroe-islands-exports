731d88ab157d0c07810d996caa8d6d712a09a0ad
Sámal Rasmussen <samal84@gmail.com>
2024-09-01 17:13:42 +0100

    Fix up view to use new data format

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 import by_month_csv from "../../by-month.csv?raw";
-import by_quarter_csv from "../../by-quarter.csv?raw";
 import groups from "$lib/country-groups.json";
 
 /**
@@ -9,99 +8,49 @@ import groups from "$lib/country-groups.json";
  * @returns {{data: string[][], header: string[]}}
  */
 function parse_csv(csv_string) {
-  const data = csv_string.split("\n").map((row) => row.split(";"));
-  data.forEach((row, i) => {
-    row.forEach((cell, j) => {
-      if (i > 0 && j > 0) {
-        row[j] = /** @type {any} */ (+cell);
-      }
-    });
-  });
+	const data = csv_string.split("\n").map((row) => row.split(";"));
+	data.forEach((row, i) => {
+		row.forEach((cell, j) => {
+			if (i > 0 && j > 0) {
+				row[j] = /** @type {any} */ (+cell);
+			}
+		});
+	});
 
-  const header = /** @type {string[]} */ (data.shift());
-  return { header, data };
+	const header = /** @type {string[]} */ (data.shift());
+	return { header, data };
 }
 
 const by_month_data = parse_csv(by_month_csv);
-const by_monts_split_header = by_month_data.header
-  .slice(1)
-  .map((month) => month.split(" "));
-const months = /** @type { const} */ ({
-  Jan: 1,
-  Feb: 2,
-  Mar: 3,
-  Apr: 4,
-  Mai: 5,
-  Jun: 6,
-  Jul: 7,
-  Aug: 8,
-  Sep: 9,
-  Okt: 10,
-  Nov: 11,
-  Des: 12,
-});
-const by_month_dates = by_monts_split_header.map((v) => {
-  const year = +v[0];
-  const month = /** @type {keyof months} */ (v[1]);
-  const date = new Date(year, months[month], 0);
-  return date;
-});
+const by_monts_split_header = by_month_data.header.slice(1).map((month) => month.split("M"));
 
-const by_quarter_data = parse_csv(by_quarter_csv);
-const by_quater_dates_split_header = by_quarter_data.header
-  .slice(1)
-  .map((quarter) => {
-    var index = quarter.indexOf(" ");
-    return [quarter.slice(0, index), quarter.slice(index + 1)];
-  });
-const quarters_to_months = /** @type { const} */ ({
-  "1. ársfj.": 3,
-  "2. ársfj.": 6,
-  "3. ársfj.": 9,
-  "4. ársfj.": 12,
-});
-const by_quarter_dates = by_quater_dates_split_header.map((v) => {
-  const year = +v[0];
-  const quarter = /** @type {keyof quarters_to_months} */ (v[1]);
-  const date = new Date(year, quarters_to_months[quarter], 0);
-  return date;
+const by_month_dates = by_monts_split_header.map((v) => {
+	const year = +v[0];
+	const month = +v[1];
+	const date = new Date(year, month, 0);
+	return date;
 });
 
 /**
  * @param {string[]} individual_countries_list
  */
 export function filter_data(
-  individual_countries_list = [
-    "Danmark",
-    "Sambandsríki Amerika (USA)",
-    "Kina",
-    "Russland",
-  ]
+	individual_countries_list = ["Danmark", "Sambandsríki Amerika (USA)", "Kina", "Russland"],
 ) {
-  const maps = get_maps(individual_countries_list);
+	const maps = get_maps(individual_countries_list);
 
-  const filtered_months_data = filter_data_internal(
-    by_month_data.data,
-    by_month_data.header,
-    maps
-  );
+	const filtered_months_data = filter_data_internal(
+		by_month_data.data,
+		by_month_data.header,
+		maps,
+	);
 
-  const filtered_quaters_data = filter_data_internal(
-    by_quarter_data.data,
-    by_quarter_data.header,
-    maps
-  );
-
-  return {
-    months_data: {
-      ...filtered_months_data,
-      dates: by_month_dates,
-    },
-    quarters_data: {
-      ...filtered_quaters_data,
-      dates: by_quarter_dates,
-    },
-  };
+	return {
+		months_data: {
+			...filtered_months_data,
+			dates: by_month_dates,
+		},
+	};
 }
 
 /**
@@ -111,79 +60,77 @@ export function filter_data(
  * @returns
  */
 function filter_data_internal(data, header, maps) {
-  const {
-    individual_countries_map,
-    nordics_map,
-    europe_map,
-    asia_map,
-    africa_map,
-    americas_map,
-  } = maps;
-  const individual_countries = data.filter((row) =>
-    individual_countries_map.has(row[0])
-  );
-  const nordics = data.filter(
-    (row) => nordics_map.has(row[0]) && !individual_countries_map.has(row[0])
-  );
-  const europe = data.filter(
-    (row) => europe_map.has(row[0]) && !individual_countries_map.has(row[0])
-  );
-  const asia = data.filter(
-    (row) => asia_map.has(row[0]) && !individual_countries_map.has(row[0])
-  );
-  const africa = data.filter(
-    (row) => africa_map.has(row[0]) && !individual_countries_map.has(row[0])
-  );
-  const americas = data.filter(
-    (row) => americas_map.has(row[0]) && !individual_countries_map.has(row[0])
-  );
+	const {
+		individual_countries_map,
+		nordics_map,
+		europe_map,
+		asia_map,
+		africa_map,
+		americas_map,
+	} = maps;
+	const individual_countries = data.filter((row) => individual_countries_map.has(row[0]));
+	const nordics = data.filter(
+		(row) => nordics_map.has(row[0]) && !individual_countries_map.has(row[0]),
+	);
+	const europe = data.filter(
+		(row) => europe_map.has(row[0]) && !individual_countries_map.has(row[0]),
+	);
+	const asia = data.filter(
+		(row) => asia_map.has(row[0]) && !individual_countries_map.has(row[0]),
+	);
+	const africa = data.filter(
+		(row) => africa_map.has(row[0]) && !individual_countries_map.has(row[0]),
+	);
+	const americas = data.filter(
+		(row) => americas_map.has(row[0]) && !individual_countries_map.has(row[0]),
+	);
 
-  const nordics_summed = sum_group(nordics, "Norðurlond");
-  const europe_summed = sum_group(europe, "Evropa");
-  const asia_summed = sum_group(asia, "Asia");
-  const africa_summed = sum_group(africa, "Afrika");
-  const americas_summed = sum_group(americas, "Amerika");
-  const all_summed = sum_group(data, "Tilsamans");
+	const nordics_summed = sum_group(nordics, "Norðurlond");
+	const europe_summed = sum_group(europe, "Evropa");
+	const asia_summed = sum_group(asia, "Asia");
+	const africa_summed = sum_group(africa, "Afrika");
+	const americas_summed = sum_group(americas, "Amerika");
+	const all_summed = sum_group(data, "Tilsamans");
 
-  return {
-    header: structuredClone(header),
-    all_summed,
-    individual_countries,
-    nordics,
-    europe,
-    asia,
-    africa,
-    americas,
-    nordics_summed,
-    europe_summed,
-    asia_summed,
-    africa_summed,
-    americas_summed,
-  };
+	return {
+		header: structuredClone(header),
+		all_summed,
+		individual_countries,
+		nordics,
+		europe,
+		asia,
+		africa,
+		americas,
+		nordics_summed,
+		europe_summed,
+		asia_summed,
+		africa_summed,
+		americas_summed,
+	};
 }
 
 /**
  * @param {string[]} individual_countries_list
  */
 function get_maps(individual_countries_list) {
-  const individual_countries_map = new Map(
-    individual_countries_list.map((country) => [country, true])
-  );
-  const nordics_map = new Map(Object.entries(groups.nordics));
-  const europe_map = new Map(Object.entries(groups.europe));
-  const asia_map = new Map(Object.entries(groups["asia-middle-east-oceania"]));
-  const africa_map = new Map(Object.entries(groups.africa));
-  const americas_map = new Map(Object.entries(groups.americas));
+	const individual_countries_map = new Map(
+		individual_countries_list.map((country) => [country, true]),
+	);
+	const nordics_map = new Map(Object.entries(groups.nordics));
+	const europe_map = new Map(Object.entries(groups.europe));
+	const asia_map = new Map(Object.entries(groups["asia-middle-east-oceania"]));
+	const africa_map = new Map(Object.entries(groups.africa));
+	const americas_map = new Map(Object.entries(groups.americas));
 
-  // return all maps
-  return {
-    individual_countries_map,
-    nordics_map,
-    europe_map,
-    asia_map,
-    africa_map,
-    americas_map,
-  };
+	// return all maps
+	return {
+		individual_countries_map,
+		nordics_map,
+		europe_map,
+		asia_map,
+		africa_map,
+		americas_map,
+	};
 }
 
 /**
@@ -191,16 +138,16 @@ function get_maps(individual_countries_list) {
  * @param {string} name
  */
 function sum_group(group, name) {
-  const reduced = group.reduce((acc, row) => {
-    for (let i = 1; i < row.length; i++) {
-      acc[i] += row[i];
-    }
-    return acc;
-  }, new Array(group[0].length).fill(0));
+	const reduced = group.reduce((acc, row) => {
+		for (let i = 1; i < row.length; i++) {
+			acc[i] += row[i];
+		}
+		return acc;
+	}, new Array(group[0].length).fill(0));
 
-  reduced.forEach((value, i) => {
-    reduced[i] = Math.round(value * 100) / 100;
-  });
-  reduced[0] = name;
-  return reduced;
+	reduced.forEach((value, i) => {
+		reduced[i] = Math.round(value * 100) / 100;
+	});
+	reduced[0] = name;
+	return reduced;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,134 +1,135 @@
 <script>
-  import { onMount } from "svelte";
-  import { filter_data } from "$lib";
+	import { onMount } from "svelte";
+	import { filter_data } from "$lib";
 
-  /**
-   * @type {HTMLDivElement}
-   */
-  let main_chart_div;
+	/**
+	 * @type {HTMLDivElement}
+	 */
+	let main_chart_div;
 
-  /**
-   * @type {HTMLDivElement}
-   */
-  let brush_chart_div;
+	/**
+	 * @type {HTMLDivElement}
+	 */
+	let brush_chart_div;
 
-  const { months_data, quarters_data } = filter_data();
+	const { months_data } = filter_data();
 
-  const {
-    dates,
-    header,
-    all_summed,
-    individual_countries,
-    nordics,
-    nordics_summed,
-    europe,
-    europe_summed,
-    asia,
-    asia_summed,
-    africa,
-    africa_summed,
-    americas,
-    americas_summed,
-  } = quarters_data;
+	const {
+		dates,
+		header,
+		all_summed,
+		individual_countries,
+		nordics,
+		nordics_summed,
+		europe,
+		europe_summed,
+		asia,
+		asia_summed,
+		africa,
+		africa_summed,
+		americas,
+		americas_summed,
+	} = months_data;
 
-  /**
-   * @param {string[]} row
-   */
-  function get_series(row) {
-    return {
-      name: row[0],
-      data: row.slice(1).map((x, i) => [dates[i], x]),
-    };
-  }
+	/**
+	 * @param {string[]} row
+	 */
+	function get_series(row) {
+		return {
+			name: row[0],
+			data: row.slice(1).map((x, i) => [dates[i], x]),
+		};
+	}
 
-  const main_chart_options = {
-    chart: {
-      id: "main_chart",
-      height: 580,
-      width: "100%",
-      type: "line",
-      animations: {
-        initialAnimation: {
-          enabled: false,
-        },
-      },
-      toolbar: {
-        autoSelected: "pan",
-        show: false,
-      },
-    },
-    stroke: {
-      width: 3,
-      curve: "smooth",
-    },
-    series: [
-      ...individual_countries.map((country) => get_series(country)),
-      get_series(nordics_summed),
-      get_series(europe_summed),
-      get_series(asia_summed),
-      get_series(africa_summed),
-      get_series(americas_summed),
-    ],
-    xaxis: {
-      type: "datetime",
-    },
-  };
+	const main_chart_options = {
+		chart: {
+			id: "main_chart",
+			height: 580,
+			width: "100%",
+			type: "line",
+			animations: {
+				initialAnimation: {
+					enabled: false,
+				},
+			},
+			toolbar: {
+				autoSelected: "pan",
+				show: false,
+			},
+		},
+		stroke: {
+			width: 3,
+			curve: "monotoneCubic",
+		},
+		series: [
+			...individual_countries.map((country) => get_series(country)),
+			get_series(nordics_summed),
+			get_series(europe_summed),
+			get_series(asia_summed),
+			get_series(africa_summed),
+			get_series(americas_summed),
+		],
+		xaxis: {
+			type: "datetime",
+		},
+	};
 
-  const max_date = dates[dates.length - 1];
-  const five_years_back = new Date();
-  five_years_back.setFullYear(max_date.getFullYear() - 5);
+	const max_date = dates[dates.length - 1];
+	const five_years_back = new Date();
+	five_years_back.setFullYear(max_date.getFullYear() - 5);
 
-  const brush_chart_options = {
-    series: [get_series(all_summed)],
-    chart: {
-      id: "brush_chart",
-      height: 130,
-      type: "area",
-      brush: {
-        target: "main_chart",
-        enabled: true,
-      },
-      selection: {
-        enabled: true,
-        xaxis: {
-          min: five_years_back.getTime(),
-          max: max_date.getTime(),
-        },
-      },
-    },
-    colors: ["#008FFB"],
-    fill: {
-      type: "gradient",
-      gradient: {
-        opacityFrom: 0.91,
-        opacityTo: 0.1,
-      },
-    },
-    xaxis: {
-      type: "datetime",
-      tooltip: {
-        enabled: false,
-      },
-    },
-    yaxis: {
-      tickAmount: 2,
-    },
-  };
+	const brush_chart_options = {
+		series: [get_series(all_summed)],
+		chart: {
+			id: "brush_chart",
+			height: 130,
+			type: "area",
+			brush: {
+				target: "main_chart",
+				enabled: true,
+			},
+			selection: {
+				enabled: true,
+				xaxis: {
+					min: five_years_back.getTime(),
+					max: max_date.getTime(),
+				},
+			},
+		},
+		colors: ["#008FFB"],
+		fill: {
+			type: "gradient",
+			gradient: {
+				opacityFrom: 0.91,
+				opacityTo: 0.1,
+			},
+		},
+		xaxis: {
+			type: "datetime",
+			tooltip: {
+				enabled: false,
+			},
+		},
+		yaxis: {
+			tickAmount: 2,
+		},
+	};
 
-  onMount(async () => {
-    const ApexCharts = (await import("apexcharts")).default;
-    /** @type {any} */ (window).ApexCharts = ApexCharts;
-    const main_chart = new ApexCharts(main_chart_div, main_chart_options);
-    main_chart.render();
-    const brush_chart = new ApexCharts(brush_chart_div, brush_chart_options);
-    brush_chart.render();
-  });
+	onMount(async () => {
+		const ApexCharts = (await import("apexcharts")).default;
+		/** @type {any} */ (window).ApexCharts = ApexCharts;
+		const main_chart = new ApexCharts(main_chart_div, main_chart_options);
+		main_chart.render();
+		const brush_chart = new ApexCharts(brush_chart_div, brush_chart_options);
+		brush_chart.render();
+	});
 </script>
 
-<span
-  >Tabellir við øllum data per: <a href="/by-month-table/months">Mánaða</a>,
-  <a href="/by-month-table/quarters">ársfjóring</a></span
->
+<span>
+	Tabellir við øllum data per:
+	<a href="/by-month-table/months">Mánaða</a>,
+	<a href="/by-month-table/quarters">ársfjóring</a>
+</span>
 
 <div bind:this={main_chart_div}></div>
 <div bind:this={brush_chart_div}></div>
