ea7e57fda53c533ef819652e31ea0d24193d0b45
Sámal Rasmussen <samal84@gmail.com>
2026-01-14 22:06:47 +0000

    Build

---
+++ docs/_app/immutable/chunks/DiXHzdqx.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_ht40sb)==null?void 0:s.base)??"/faroe-islands-exports";var t;const e=((t=globalThis.__sveltekit_ht40sb)==null?void 0:t.assets)??a;export{e as a,a as b};
--- docs/_app/immutable/entry/start.CGrePCFT.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/Dys-NWOj.js";export{o as load_css,r as start};
